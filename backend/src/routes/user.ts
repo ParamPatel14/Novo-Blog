86212c9531c85cd2fcd63cfb60e3f057e3f06e47
ParamPatel14 <parampraman59@gmail.com>
2025-07-16 21:14:51 +0530

    Final All Done

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -4,68 +4,82 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { sign } from "hono/jwt";
 
-
 export const userRouter = new Hono<{
-  Bindings: {
-    DATABASE_URL: string
-    JWT_SECRET: string
-  }
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
 }>();
 
 userRouter.post('/signup', async (c) => {
-  const body = await c.req.json();
-  const { success } = signupInput.safeParse(body);
-  if (!success) {
-    c.status(400);
-    return c.json({ error: "Invalid input" });
-  }
-
+    const body = await c.req.json();
+    const { success } = signupInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs not correct"
+        })
+    }
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
   
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
+    try {
+      const user = await prisma.user.create({
+        data: {
+          email: body.email,
+          password: body.password,
+          name: body.name
+        }
+      })
+      const jwt = await sign({
+        id: user.id
+      }, c.env.JWT_SECRET);
+  
+      return c.text(jwt)
+    } catch(e) {
+      console.log(e);
+      c.status(411);
+      return c.text('Invalid')
+    }
+  })
+  
+  
+  userRouter.post('/signin', async (c) => {
+    const body = await c.req.json();
+    const { success } = signinInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({
+            message: "Inputs not correct"
+        })
+    }
 
-  try {
-		const user = await prisma.user.create({
-			data: {
-				email: body.email,
-				password: body.password
-			}
-		});
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-		return c.json({ jwt });
-	} catch(e) {
-		c.status(403);
-    return c.json({ error: "error while signing up" });
-	}
-})
-
-userRouter.post('signin', async (c) => {
-  const body = await c.req.json();
-  const { success } = signinInput.safeParse(body);
-  if (!success) {
-    c.status(400);
-    return c.json({ error: "Invalid input" });
-  }
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-  try{
-    const user = await prisma.user.findUnique({
-      where: {
-        email: body.email,
-        password: body.password
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          email: body.email,
+          password: body.password,
+        }
+      })
+      if (!user) {
+        c.status(403);
+        return c.json({
+          message: "Incorrect creds"
+        })
       }
-    });
-    if (!user) {
-		c.status(403);
-		return c.json({ error: "user not found" });
-	  }
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-	  return c.json({ jwt });
-  } catch(e) {
-    c.status(403);
-    return c.json({ error: "error while signing in" });
-  }
-})
+      const jwt = await sign({
+        id: user.id
+      }, c.env.JWT_SECRET);
+  
+      return c.text(jwt)
+    } catch(e) {
+      console.log(e);
+      c.status(411);
+      return c.text('Invalid')
+    }
+  })
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.darkninja.workers.dev"
